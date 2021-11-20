06299d03df126b2646169dc2be483064c6f26681
raikasdev <roni.aikas@gmail.com>
2021-11-20 16:01:50 +0200

    Minify js

--- src/index.js
+++ src/index.js
@@ -9,12 +9,11 @@ const envRegex = /(^\s*\S*=).*$/gm;
 
 try {
     if (fs.existsSync(envPath)) {
-      console.log('Generating .env.example...');
       const data = fs.readFileSync(envPath, { encoding:'utf8', flag: 'r' });
       fs.writeFileSync(envExamplePath, data.replaceAll(envRegex, '$1'));
       console.log('.env.example generated')
     } else {
-        console.log('No .env file found in folder ' + process.cwd());
+        console.log('.env not found in' + process.cwd());
     }
 } catch(err) {
     console.error(err)
