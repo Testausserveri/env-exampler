dfed8b08a3c9da3d5086f725dccce5c1494223e9
Luukas Pörtfors <lajp@iki.fi>
2021-11-20 15:39:52 +0200

    Fix #2

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 #!/usr/bin/env node
-const 
+const
  fs = require('fs'),
  path = require('path');
 
 const envPath = path.resolve(process.cwd(), '.env');
 const envExamplePath = path.resolve(process.cwd(), '.env.example');
-const envRegex = /(^[A-Z0-9_]+)(\=)(.*(\n)(?=[A-Z])|.*$)/gm;
+const envRegex = /(^\s*\S*=).*$/gm;
 
 try {
     if (fs.existsSync(envPath)) {
       console.log('Generating .env.example...');
       const data = fs.readFileSync(envPath, { encoding:'utf8', flag: 'r' });
-      fs.writeFileSync(envExamplePath, data.replaceAll(envRegex, '$1$2$4'));
+      fs.writeFileSync(envExamplePath, data.replaceAll(envRegex, '$1'));
       console.log('.env.example generated')
     } else {
         console.log('No .env file found in folder ' + process.cwd());
